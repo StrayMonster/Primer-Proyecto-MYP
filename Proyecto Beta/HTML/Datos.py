2b277f152e566009462220d8a584ebcdc71da27e
StrayMonster <alan_lucero@ciencias.unam.mx>
2024-09-01 18:30:18 -0600

    Full Stack 6 / n
    En este archivo fue el mejor avance que se pudo haber tenido, finalmente se pudo ejecutar con éxito rotundo el flask. Igual se implemento una mejora en el sistema (en la vida real se tiene que consultar antes y cobrar extra) puesto que se escribieron las opciones de origen y destino posibles. Pero busca las ubicaciones que sean, sin necesidad de estar acomodas como en la base de datos. Es genial.
    -Se cambió de posición varios archivos para el funcionamiento del flask.
    -Se muestran los resultados en el front2 los que fueron elegidos.
    
    Sigue el último paso, la implementación del flask.

--- Proyecto Beta/HTML/Datos.py
+++ Proyecto Beta/HTML/Datos.py
@@ -1,41 +1,51 @@
 from flask import Flask, request, render_template, redirect, url_for
 import pandas as pd
 
-app =  Flask(__name__)
+app = Flask(__name__)
+
+#Vincular Base de datos con este programa:
+#Se le pide por favor que ingrese su dirección de archivo local para que funcione esté elemento.
+ubicaciones = pd.read_csv('E:/Github/Primer-Proyecto-MYP/Proyecto Beta/HTML/static/Recursos/ubicaciones.csv')
 
 @app.route('/')
-def formulario():
-    return render_template('formulario.html')
+def index():
+    return render_template('Front.html')
 
 @app.route('/procesar', methods=['POST'])
 def procesar():
-    origenForm = request.form['Parametro1']
-    destinoForm = request.form['Parametro2']
+    origen = request.form['Parametro1']
+    destino = request.form['Parametro2']
 
-    #Vincular Base de datos con este programa:
-    #Se le pide por favor que ingrese su dirección de archivo local para que funcione esté elemento.
-    ubicaciones = pd.read_csv('E:/Github/Primer-Proyecto-MYP/Proyecto Beta/HTML/Recursos/ubicaciones.csv')
-    print(ubicaciones.head())
+    filtroExacto = ubicaciones[(ubicaciones['origin'] == origen) & (ubicaciones['destination'] == destino)]
 
-    filtroReal = ubicaciones[(ubicaciones['origin'] == 'MEX') & (ubicaciones['destination'] == 'MTY')]
+    if not filtroExacto.empty:
+        primerElemento = filtroExacto.iloc[0]
+        datos = {
+            'origenLatitud': primerElemento['origin_latitude'],
+            'origenLongitud': primerElemento['origin_longitude'],
+            'destinoLatitud': primerElemento['destination_latitude'],
+            'destinoLongitud': primerElemento['destination_longitude'],
+            'origen': primerElemento['origin'],
+            'destino': primerElemento['destination']
+        }
+    else:
+        filtroOrigen = ubicaciones[ubicaciones['origin'] == origen]
+        filtroDestino = ubicaciones[ubicaciones['destination'] == destino]
 
-    if not filtroReal.empty:
-        primerElemento = filtroReal.iloc[0]
-        origenLatitud = primerElemento['origin_latitude']
-        origenLongitud = primerElemento['origin_longitude']
-        destinoLatitud = primerElemento['destination_latitude']
-        destinoLongitud = primerElemento['destination_longitude']
-        origen = primerElemento['origin']
-        destino = primerElemento['destination']
-    filtroReal.to_csv('HTML/Recursos/resultado.csv', index = False)
-    return redirect(url_for('resultado'))
+        datos = {
+            'origenLatitud': filtroOrigen.iloc[0]['origin_latitude'] if not filtroOrigen.empty else "No disponoble",
+            'origenLongitud': filtroOrigen.iloc[0]['origin_longitude'] if not filtroOrigen.empty else "No disponible",
+            'destinoLatitud': filtroDestino.iloc[0]['destination_latitude'] if not filtroDestino.empty else "No disponible",
+            'destinoLongitud': filtroDestino.iloc[0]['destination_longitude'] if not filtroDestino.empty else "No disponible",
+            'origen': origen,
+            'destino': destino
+        }
+    return redirect(url_for('resultado', **datos))
 
 @app.route('/resultado')
 def resultado():
-    ubicacionesH = pd.read_csv('E:/Github/Primer-Proyecto-MYP/Proyecto Beta/HTML/Recursos/ubicaciones.csv') 
-    tablaHtml = ubicacionesH.to_html()
-    return render_template('Front2.html', tabla = tablaHtml)
+    datos = request.args
+    return render_template('Front2.html', datos = datos)
 
 if __name__ == '__main__':
     app.run(debug = True)
-
